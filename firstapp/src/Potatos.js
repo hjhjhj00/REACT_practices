45b7b7b40138d7b9ec54c8fbe2aa73acbb2bc390
hjhjhj00 <dlapdlffldpald1@naver.com>
2021-09-30 05:04:46 +0900

    firstapp

---
+++ firstapp/src/Potatos.js
@@ -0,0 +1,7 @@
+//import React from "react";
+
+function Potatos(){
+    return <div>It's potato!</div>;
+}
+
+export default Potatos;
---
+++ firstapp/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//import Potato from './Potato.js';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+//ReactDOM.render(<Potato />,document.querySelector("#root"));
+//Potato가 덮어쓰는 것 확인.
