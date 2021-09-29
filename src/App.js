36c6a22153d5231d8af85d0e1e1b69f86e3f803d
jungminyoo <yoojm4718@gmail.com>
2021-09-30 01:56:15 +0900

    Finished Assignment of Week 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,63 @@
-function App() {
-  return <div className="App"></div>;
+import React from "react";
+
+const localStorage = window.localStorage;
+
+class App extends React.Component {
+  state = {
+    toDos: [],
+    input: "",
+  };
+  componentDidMount() {
+    if (localStorage.getItem("toDos")) {
+      this.setState({ toDos: JSON.parse(localStorage.getItem("toDos")) });
+    }
+  }
+  onSubmit = (event) => {
+    event.preventDefault();
+    const newToDos = [
+      ...this.state.toDos,
+      { id: Date.now(), data: this.state.input },
+    ];
+    this.setState({
+      toDos: newToDos,
+      input: "",
+    });
+    localStorage.setItem("toDos", JSON.stringify(newToDos));
+  };
+  deleteItem = (event) => {
+    const parentId = event.target.parentNode.id;
+    const deletedToDos = this.state.toDos.filter(
+      (toDo) => toDo.id !== parseInt(parentId)
+    );
+    this.setState({ toDos: deletedToDos });
+    localStorage.setItem("toDos", JSON.stringify(deletedToDos));
+  };
+  render() {
+    const { toDos, input } = this.state;
+    return (
+      <>
+        <h1>To Do</h1>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            value={input}
+            onChange={(event) => {
+              this.setState({ input: event.target.value });
+            }}
+          />
+          <button>Add</button>
+        </form>
+        <ul>
+          {toDos.map((toDo, index) => (
+            <li key={toDo.id} id={toDo.id}>
+              {toDo.data}
+              <button onClick={this.deleteItem}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
