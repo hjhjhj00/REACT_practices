dfb1b1365fccc6027b2a4cf92f89c9653cd4ccc3
jungminyoo <yoojm4718@gmail.com>
2021-09-30 13:13:23 +0900

    Added Comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const localStorage = window.localStorage;
+const localStorage = window.localStorage; // localStorage 전역변수 설정
 
 class App extends React.Component {
   state = {
@@ -8,26 +8,30 @@ class App extends React.Component {
     input: "",
   };
   componentDidMount() {
+    // 컴포넌트 생성시 this.state.toDos에 localStorage의 데이터 불러오기
     if (localStorage.getItem("toDos")) {
-      this.setState({ toDos: JSON.parse(localStorage.getItem("toDos")) });
+      // 투두가 존재 하는 경우
+      this.setState({ toDos: JSON.parse(localStorage.getItem("toDos")) }); // string -> JSON
     }
   }
   onSubmit = (event) => {
     event.preventDefault();
     const newToDos = [
       ...this.state.toDos,
-      { id: Date.now(), data: this.state.input },
+      { id: Date.now(), data: this.state.input }, // 각 toDo의 요소: id, data
     ];
     this.setState({
+      // state에 업데이트
       toDos: newToDos,
       input: "",
     });
-    localStorage.setItem("toDos", JSON.stringify(newToDos));
+    localStorage.setItem("toDos", JSON.stringify(newToDos)); // localStorage에도 업데이트
   };
   deleteItem = (event) => {
     const parentId = event.target.parentNode.id;
     const deletedToDos = this.state.toDos.filter(
-      (toDo) => toDo.id !== parseInt(parentId)
+      // 해당 id를 가진 데이터를 제외한 나머지만 추출
+      (toDo) => toDo.id !== parseInt(parentId) // Node에서 가지고 온 데이터는 string
     );
     this.setState({ toDos: deletedToDos });
     localStorage.setItem("toDos", JSON.stringify(deletedToDos));
@@ -48,7 +52,7 @@ class App extends React.Component {
           <button>Add</button>
         </form>
         <ul>
-          {toDos.map((toDo, index) => (
+          {toDos.map((toDo) => (
             <li key={toDo.id} id={toDo.id}>
               {toDo.data}
               <button onClick={this.deleteItem}>Delete</button>
