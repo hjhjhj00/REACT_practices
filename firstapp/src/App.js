c4a01223949683ab59340c08e1ce3ae5318ae5a1
hjhjhj00 <dlapdlffldpald1@naver.com>
2021-11-11 20:14:18 +0900

    todos_pre.

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { createElement } from 'react';
 
 class App extends React.Component {
   state={value: ""};
 
+  componentDidMount(){
+    const form = window.document.querySelector("form");
+    form.addEventListener("submit",(event) => {
+      event.preventDefault();
+      console.log(this.state.value);
+      //const span = React.createElement("span");
+      //span.innerText=this.state.value;
+    })
+  }
+
   onChange = (event) =>{
-    let val = event.target.value;
-    console.log(val);
+    this.setState({value: event.target.value})
+    //console.log(this.state.value);
   }
 
   enter = (event) => {
     //this.setState({value: "hi"});
     console.log(this.state.value);
-    console.log("enter");};
+    console.log("enter");
+    let span = document.createElement("span");
+    span.innterText= this.state.value;
+
+  };
 
   render(){
     return(
       //<form onsubmit={input.value}>
         //<input></input>
       //</form>
+   
       <>
+      <form>
       <input value={this.state.value} onChange={this.onChange}></input>
+      </form>
       <button onClick={this.enter}>enter</button>
       </>
     );
